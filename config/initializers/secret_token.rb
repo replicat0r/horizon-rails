ab8607d23cd2809cc402c165fcc93426f8372965
tyler liu <replicat0r.misc@gmail.com>
2015-04-20 22:28:03 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HorizonRails::Application.config.secret_key_base = 'c4db2a2bb0aa56cf442b118fe1d2a67dd6a0a384365f675dc6df34a30ff9ec2595e054a2bda230dc344160e04db4a7d8131fed5b340f03ead4d663b0c683be5e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HorizonRails::Application.config.session_store :cookie_store, key: '_horizon-rails_session'
