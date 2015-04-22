143cec2ce2575a6d77f109513053b02f1dbe2d3d
tyler liu <replicat0r.misc@gmail.com>
2015-04-22 02:11:54 -0400

    added html compressor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,31 @@ HorizonRails::Application.configure do
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
+  config.middleware.use Rack::Deflater
+  config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
+
+  config.middleware.use HtmlCompressor::Rack,
+    compress_css: true,
+    compress_javascript: true,
+    css_compressor: Sass,
+    enabled: true,
+    javascript_compressor: uglifier,
+    preserve_line_breaks: false,
+    remove_comments: true,
+    remove_form_attributes: false,
+    remove_http_protocol: false,
+    remove_https_protocol: false,
+    remove_input_attributes: true,
+    remove_intertag_spaces: false,
+    remove_javascript_protocol: true,
+    remove_link_attributes: true,
+    remove_multi_spaces: true,
+    remove_quotes: true,
+    remove_script_attributes: true,
+    remove_style_attributes: true,
+    simple_boolean_attributes: true,
+    simple_doctype: false
+end
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
